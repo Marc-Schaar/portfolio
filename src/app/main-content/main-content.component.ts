59ae0d22d1244fa3c93bea6dd403e9840aec497e
Marc Schaar <m-schaar@gmx.net>
2024-12-22 16:41:27 +0100

    update atf

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { AtfComponent } from './atf/atf.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [AtfComponent],
   templateUrl: './main-content.component.html',
-  styleUrl: './main-content.component.scss'
+  styleUrl: './main-content.component.scss',
 })
-export class MainContentComponent {
-
-}
+export class MainContentComponent {}
