1ae1edc92a0261c78557de572a628b9510007cd4
Marc Schaar <m-schaar@gmx.net>
2024-12-28 13:40:21 +0100

    add prtfilio and referneces

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,11 +2,19 @@ import { Component, AfterViewInit } from '@angular/core';
 import { AtfComponent } from './atf/atf.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ReferencesComponent } from './references/references.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [AtfComponent, AboutMeComponent, SkillsComponent],
+  imports: [
+    AtfComponent,
+    AboutMeComponent,
+    SkillsComponent,
+    PortfolioComponent,
+    ReferencesComponent,
+  ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss',
 })
