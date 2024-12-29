3348d8a9d9bdb50d69df9b776258e0bc0e8915b3
Marc Schaar <m-schaar@gmx.net>
2024-12-29 12:22:46 +0100

    add portfolio line

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -4,6 +4,7 @@ import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillsComponent } from './skills/skills.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ReferencesComponent } from './references/references.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-main-content',
@@ -14,6 +15,7 @@ import { ReferencesComponent } from './references/references.component';
     SkillsComponent,
     PortfolioComponent,
     ReferencesComponent,
+    ContactComponent,
   ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss',
