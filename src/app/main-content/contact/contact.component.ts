8237e8368c384f03a12968e4440e0b28bd7bef7e
Marc Schaar <m-schaar@gmx.net>
2024-12-30 08:55:09 +0100

    add contact and update bg objects

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,12 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
+  form: { name: string; email: string; message: string } = {
+    name: '',
+    email: '',
+    message: '',
+  };
 
+  #name: string = '';
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.valid && ngForm.submitted) {
+      console.log('Form submitted:', this.form);
+      ngForm.resetForm();
+      this.form = {
+        name: '',
+        email: '',
+        message: '',
+      };
+    }
+  }
 }
