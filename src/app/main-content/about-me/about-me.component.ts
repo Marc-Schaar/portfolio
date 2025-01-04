3f1fe742c5145b65b1ae67dce4e3205c96b5c17f
Marc Schaar <m-schaar@gmx.net>
2025-01-04 22:56:03 +0100

    add portfolio section translation

--- src/app/main-content/about-me/about-me.component.ts
+++ src/app/main-content/about-me/about-me.component.ts
@@ -1,11 +1,10 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [CommonModule, TranslateModule],
+  imports: [TranslateModule],
   templateUrl: './about-me.component.html',
   styleUrls: [
     './about-me.component.scss',
