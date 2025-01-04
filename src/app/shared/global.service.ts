c44f4df81c82b5413ea5632ab311b82b9530a258
Marc Schaar <m-schaar@gmx.net>
2025-01-04 22:28:07 +0100

    clean up translation function

--- src/app/shared/global.service.ts
+++ src/app/shared/global.service.ts
@@ -1,36 +1,23 @@
 import { Injectable } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GlobalService {
-  // private languageSubject = new BehaviorSubject<string>('en'); // Standardsprache 'en'
-
   constructor(private translate: TranslateService) {
-    // Standardsprache setzen
-    this.translate.setDefaultLang('en');
-
-    // Automatisch die Sprache des Browsers verwenden oder Standard setzen
     let browserLang = this.translate.getBrowserLang();
     let initialLang = browserLang?.match(/en|de/) ? browserLang : 'en';
     this.translate.use(initialLang);
-    // this.languageSubject.next(initialLang); // Sprache bekannt geben
   }
 
-  scrollToTop(): void {
+  scrollToTop() {
     window.scrollTo({
       top: 0,
     });
   }
 
-  changeLanguage(lang: string): void {
-    this.translate.use(lang); // Sprache ändern
-    // this.languageSubject.next(lang); // Änderung bekannt geben
+  changeLanguage(lang: string) {
+    this.translate.use(lang);
   }
-
-  // getCurrentLanguage(): string {
-  //   return this.translate.currentLang; // Aktuelle Sprache zurückgeben
-  // }
 }
