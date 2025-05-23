58aa8873ca0f1f74106be67c67f37d2e092c5ccd
Marc Schaar <m-schaar@gmx.net>
2025-05-23 19:45:08 +0200

    update: header navabr mobile style

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,19 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GlobalService } from '../global.service';
 import { TranslateModule } from '@ngx-translate/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { fromEvent, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [TranslateModule, RouterLink],
+  imports: [TranslateModule, RouterLink, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   constructor(private globalService: GlobalService) {}
   isOpen = false;
   currentLang = this.globalService.getCurrentLanguage();
+
+  resizeSubscription!: Subscription;
+
+  ngOnInit(): void {
+    this.handleResize(); // Initial check
+
+    this.resizeSubscription = fromEvent(window, 'resize').subscribe(() =>
+      this.handleResize()
+    );
+  }
 
   scroll() {
     this.globalService.scrollToTop();
@@ -32,5 +44,20 @@ export class HeaderComponent {
   changeLanguage(lang: string): void {
     this.globalService.changeLanguage(lang);
     this.currentLang = lang;
+  }
+
+  handleResize() {
+    const width = window.innerWidth;
+    if (width < 1000) {
+      if (!this.isOpen) {
+        this.isOpen = false;
+        document.body.style.overflow = '';
+      }
+    } else {
+      if (this.isOpen) {
+        this.isOpen = false;
+        document.body.style.overflow = '';
+      }
+    }
   }
 }
